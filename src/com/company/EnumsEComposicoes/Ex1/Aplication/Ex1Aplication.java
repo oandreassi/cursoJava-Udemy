27a215b748b8201958c4d31e865814d5d533a53c
Matheus Andreassi <andreassidev@gmail.com>
2022-02-22 18:30:44 -0300

    Adicionando primeiro exercicio de enums e composicoes

---
+++ src/com/company/EnumsEComposicoes/Ex1/Aplication/Ex1Aplication.java
@@ -0,0 +1,8 @@
+package com.company.EnumsEComposicoes.Ex1.Aplication;
+
+public class Ex1Aplication {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/company/EnumsEComposicoes/Ex1/Entities/WorkerLevel.java
@@ -0,0 +1,7 @@
+package com.company.EnumsEComposicoes.Ex1.Entities;
+
+public enum WorkerLevel {
+    JUNIOR,
+    MID_LEVEL,
+    SENIOR;
+}
